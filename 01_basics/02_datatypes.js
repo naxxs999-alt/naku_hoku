8ea7cd3a06e50e144db2c470890b28a75ada9604
naxxs999-alt <naxxs999@gmail.com>
2025-09-03 05:15:16 +0000

    conversion of data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";   // treat all js codes as newer version
+
+//alert("3 + 3") // we are using mode js not browser 
+
+console.log(3 +
+    
+    3 )  // this is not a good practice code readability should be high 
+
+console.log("nakul")
+
+let name = "nakul"
+let age = 18
+let isLoggedIn = false
+
+
+//number => 2 to pwer 53
+//bigint
+//string => ""
+//boolean => true/false 
+//null => standalone value 
+//undefined 
+// symbole => unique
+
+//object 
+
+console.log(typeof null);  // object 
+console.log(undefined);    //undefiend 
+
+
+
